2efc8221b0be67a576db55e39352f653709302a1
Martin Fitzner <martin.fitzner@roche.com>
2020-11-01 17:43:07 +0100

    added multiple independent graphs support

--- attentivefp/models/training.py
+++ attentivefp/models/training.py
@@ -59,9 +59,10 @@ def train_single_epoch2(model, data_loader, loss_criterion, optimizer, device):
         prediction = model(bg1, bg2, bg3,
                            bg1.ndata['hv'], bg2.ndata['hv'], bg3.ndata['hv'],
                            bg1.edata['e'], bg2.edata['e'], bg3.edata['e'])
-        print('\n\n AAA: ', prediction[0:5, 0])
-        print('\n\n BBB: ', prediction[0:5, 1])
-        print('\n\n CCC: ', prediction[0:5, 2])
+        print('\n\n 000: ', prediction.shape)
+        print('\n\n AAA: ', prediction[0, 0])
+        print('\n\n BBB: ', prediction[0, 1])
+        print('\n\n CCC: ', prediction[0, 2])
 
         # Handle qualified values.
         # Mask -1 = >, -2 = <
